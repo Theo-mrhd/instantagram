c570b7fe179807c58e565bf35f56b5aa05d9d60b
Theomrh <92977368+Theomrh@users.noreply.github.com>
2024-10-25 13:31:11 +0200

    Add delete message

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import {
   createMessage,
+  deleteMessage,
   getDiscussion,
   getMessages,
 } from "../controller/message.controller.js";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/messages/discussion", verifyToken, getDiscussion);
 router.get("/messages/:receiverId", verifyToken, getMessages);
 router.post("/messages", verifyToken, createMessage);
+router.delete("/message/:messageId", verifyToken, deleteMessage);
 
 export default router;
